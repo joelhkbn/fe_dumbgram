91c563d5436d5b6f6858faea55c66dc63c73003d
Joel Hukubun <joelhukubun@gmail.com>
2022-01-14 21:31:12 +0800

    upload first time

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react'
+import Landing from './pages/Landing'
+
+function App() {
+  return (
+    <div>
+      <Landing />
+    </div>
+  )
+}
+
+export default App
